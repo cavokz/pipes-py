697807f656981c10f5e398592d0734f199eea60c
Domenico Andreoli <domenico.andreoli@elastic.co>
2025-01-10 15:28:46 +0100

    Initial implementation

---
+++ core/import.py
@@ -0,0 +1,36 @@
+# Copyright 2025 Elasticsearch B.V.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Elastic Pipes component to import data into the Pipes state."""
+
+from . import Pipe
+from .util import deserialize_yaml, set_field
+
+
+@Pipe("elastic.pipes.core.import")
+def main(pipe, dry_run=False):
+    file_name = pipe.config("file")
+    field = pipe.config("field", None)
+
+    if dry_run:
+        return
+
+    with open(file_name, "r") as f:
+        value = deserialize_yaml(f)
+
+    if field in (None, "", "."):
+        pipe.logger.info(f"importing everything from '{file_name}'...")
+    else:
+        pipe.logger.info(f"importing '{field}' from '{file_name}'...")
+    set_field(pipe.state, field, value)
---
+++ runner/__init__.py
@@ -0,0 +1,106 @@
+#!/usr/bin/env python3
+
+# Copyright 2025 Elasticsearch B.V.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import sys
+from pathlib import Path
+
+import typer
+from typing_extensions import Annotated
+
+main = typer.Typer(pretty_exceptions_enable=False)
+
+
+def read_config(config_file):
+    import yaml
+
+    with open(config_file) as f:
+        return yaml.safe_load(f) or {}
+
+
+def setup_logging():
+    import logging
+
+    # a single handler to rule them all
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter("%(name)s - %(message)s"))
+    # root of all the elastic.pipes.* loggers
+    logger = logging.getLogger("elastic.pipes")
+    # all the pipes sync their handlers with this
+    logger.addHandler(handler)
+    # all the pipes sync their log level with this, unless configured differently
+    logger.setLevel(logging.INFO)
+
+
+@main.command()
+def run(
+    config_file: Path,
+    dry_run: Annotated[bool, typer.Option()] = False,
+):
+    """
+    Run pipes
+    """
+    from ..core import Pipe
+    from ..core.errors import Error
+
+    setup_logging()
+    state = read_config(config_file)
+
+    base_dir = str(config_file.parent.absolute())
+    if base_dir not in sys.path:
+        sys.path.append(base_dir)
+
+    state.setdefault("runtime", {})["base-dir"] = base_dir
+
+    try:
+        Pipe.run(state, dry_run=dry_run)
+    except Error as e:
+        print(e, file=sys.stderr)
+        sys.exit(1)
+
+
+@main.command()
+def new(
+    pipe_file: Path,
+    force: Annotated[bool, typer.Option("--force", "-f")] = False,
+):
+    """
+    Create a new pipe module
+    """
+
+    pipe_file = pipe_file.with_suffix(".py")
+
+    try:
+        with pipe_file.open("w" if force else "x") as f:
+            f.write(
+                f"""#!/usr/bin/env python3
+
+from elastic.pipes.core import Pipe
+
+@Pipe("{pipe_file.stem}", default={{}})
+def main(pipe, dry_run=False):
+    pipe.logger.info("Hello, world!")
+
+if __name__ == "__main__":
+    main()
+"""
+            )
+    except FileExistsError:
+        print(f"File already exists: '{pipe_file}'")
+        sys.exit(1)
+
+    # make it executable
+    mode = pipe_file.stat().st_mode
+    pipe_file.chmod(mode | 0o111)
